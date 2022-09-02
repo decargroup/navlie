6074e9dbbd60bad43562e7ac90df892aaa390044
Charles Cossette <charles.cossette@mail.mcgill.ca>
2022-09-02 09:24:21 -0400

    added some doc in utils

--- pynav/utils.py
+++ pynav/utils.py
@@ -13,7 +13,6 @@ class GaussianResult:
     about a Gaussian filter's state estimate, given the ground-truth value of
     the state.
     """
-
     __slots__ = [
         "stamp",
         "state",
@@ -29,30 +28,49 @@ class GaussianResult:
     def __init__(
         self,
         estimate: StateWithCovariance,
-        state_true: State = None,
+        state_true: State,
     ):
+        """
+        Parameters
+        ----------
+        estimate : StateWithCovariance
+            Estimated state and corresponding covariance.
+        state_true : State
+            The true state, which will be used to compute various error metrics.
+        """
+        
         state = estimate.state
         covariance = estimate.covariance
+
+        #:float: timestamp 
         self.stamp = state.stamp
+        #:State: estimated state 
         self.state = state
+        #:State: true state
         self.state_true = state_true
+        #:numpy.ndarray: covariance associated with estimated state
         self.covariance = covariance
-        if state_true is not None:
-            e = state.minus(state_true).reshape((-1, 1))
-            cov_inv = np.linalg.inv(covariance)
-            self.error = e.flatten()
-            self.ees = np.ndarray.item(e.T @ e)
-            self.nees = np.ndarray.item(e.T @ cov_inv @ e)
-            self.md = np.sqrt(self.nees)
-            self.three_sigma = 3 * np.sqrt(np.diag(covariance))
+
+        e = state.minus(state_true).reshape((-1, 1))
+        cov_inv = np.linalg.inv(covariance)
+        #:numpy.ndarray: error vector between estimated and true state
+        self.error = e.flatten()
+        #:float: sum of estimation error squared (EES)
+        self.ees = np.ndarray.item(e.T @ e)
+        #:float: normalized estimation error squared (NEES)
+        self.nees = np.ndarray.item(e.T @ cov_inv @ e)
+        #:float: Mahalanobis distance
+        self.md = np.sqrt(self.nees)
+        #:numpy.ndarray: three-sigma bounds on each error component
+        self.three_sigma = 3 * np.sqrt(np.diag(covariance))
 
 
 class GaussianResultList:
     """
     A data container that accepts a list of `GaussianResult` objects and
-    stacks the attributes of each object numpy arrays. Convenient for plotting.
+    stacks the attributes in numpy arrays. Convenient for plotting. This object
+    does nothing more than array-ifying the attributes of `GaussianResult`
     """
-
     __slots__ = [
         "stamp",
         "state",
@@ -67,19 +85,39 @@ class GaussianResultList:
         "value_true",
         "dof",
     ]
-
     def __init__(self, result_list: List[GaussianResult]):
+        """
+        Parameters
+        ----------
+        result_list : List[GaussianResult]
+            An entire sequence/trajectory of GaussianResult, typically of an 
+            entire estimation trial.
+
+        Let `N = len(result_list)`
+        """
+        #:numpy.ndarray with shape (N,):  timestamp 
         self.stamp = np.array([r.stamp for r in result_list])
+        #:numpy.ndarray with shape (N,): numpy array of State objects
         self.state = np.array([r.state for r in result_list])
+        #:numpy.ndarray with shape (N,): numpy array of true State objects
         self.state_true = np.array([r.state_true for r in result_list])
+        #:numpy.ndarray with shape (N,dof,dof): covariance
         self.covariance = np.array([r.covariance for r in result_list])
+        #:numpy.ndarray with shape (N, dof): error throughout trajectory
         self.error = np.array([r.error for r in result_list])
+        #:numpy.ndarray with shape (N,): EES throughout trajectory 
         self.ees = np.array([r.ees for r in result_list])
+        #:numpy.ndarray with shape (N,): NEES throughout trajectory 
         self.nees = np.array([r.nees for r in result_list])
+        #:numpy.ndarray with shape (N,): Mahalanobis distance throughout trajectory         
         self.md = np.array([r.md for r in result_list])
+        #:numpy.ndarray with shape (N, dof): three-sigma bounds
         self.three_sigma = np.array([r.three_sigma for r in result_list])
+        #:numpy.ndarray with shape (N,): state value. type depends on implementation
         self.value = np.array([r.state.value for r in result_list])
+        #:numpy.ndarray with shape (N,): dof throughout trajectory
         self.dof = np.array([r.state.dof for r in result_list])
+        #:numpy.ndarray with shape (N,): true state value. type depends on implementation
         self.value_true = np.array([r.state_true.value for r in result_list])
 
 
@@ -91,6 +129,14 @@ class MonteCarloResult:
     """
 
     def __init__(self, trial_results: List[GaussianResultList]):
+        """
+
+        Parameters
+        ----------
+        trial_results : List[GaussianResultList]
+            Each GaussianResultList corresponds to a trial. This object assumes
+            that the timestamps in each trial are identical.
+        """
         self.num_trials = len(trial_results)
         self.stamp = trial_results[0].stamp
         self.average_nees = np.average(
