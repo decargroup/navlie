17374c08a19efd4c75f94330c9cf9aed8d3535b0
Charles Cossette <charles.cossette@mail.mcgill.ca>
2022-09-30 16:11:17 -0400

    IMU preintegration now equivalent to dead reckoning.

--- pynav/lib/preintegration.py
+++ pynav/lib/preintegration.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
-from pynav.types import State
+from typing import List
+from pynav.types import StampedValue, State, StateWithCovariance
 from pynav.lib.imu import (
     IMU,
     IMUState,
     U_matrix,
     adjoint_IE3,
-    get_unbiased_imu,
     inverse_IE3,
     L_matrix,
+    G_matrix
 )
 import numpy as np
 from pylie import SE23
@@ -26,33 +27,51 @@ class RelativeMotionIncrement(ABC):
         #:np.ndarray: the covariance matrix of the RMI
         self.covariance = None
 
-    @staticmethod
-    def from_measurements(u_list, x_list):
+    
+    def increment_many(
+        self, u_list: List[StampedValue], end_stamp: float = None
+    ) -> "RelativeMotionIncrement":
         """
+        Creates an RMI object directly from a list of input measurements.
+        The two timestamps i, j are inferred from the first and last measurements
+        in the list, unless the optional `end_stamp` argument is provided.
+        In this case, the ending timestamp is set to `end_stamp` and the
+        last input in the list is used to increment the RMI from `u_list[-1].stamp`
+        to `end_stamp`.
+
         Parameters
         ----------
         u_list : List[StampedValue]
             List of input measurements to construct RMI from
-        x_list : List[State]
-            List of state measurements to construct RMI from
+        end_stamp : float, optional
+            final timestamp of RMI, by default None
 
         Returns
         -------
         RelativeMotionIncrement
             The RMI
         """
-        pass
+        
+        for k in range(len(u_list) - 1):
+            dt = u_list[k + 1].stamp - u_list[k].stamp
+            self.increment(u_list[k].value, dt)
+
+        if end_stamp is not None:
+            dt = end_stamp - u_list[-1].stamp
+            self.increment(u_list[-1].value, dt)
+
+        return self
 
     @abstractmethod
     def increment(self, u, dt):
         """
-        Updates the RMI given an input measurement `u` and a duration `dt`
+        In-place updating the RMI given an input measurement `u` and a duration `dt`
         over which to preintegrate.
         """
         pass
 
     @abstractmethod
-    def apply_to_state(self, x: State):
+    def predict(self, x: StateWithCovariance) -> StateWithCovariance:
         """
         Parameters
         ----------
@@ -73,6 +92,7 @@ class IMUIncrement(RelativeMotionIncrement):
         input_covariance: np.ndarray,
         gyro_bias: np.ndarray,
         accel_bias: np.ndarray,
+        gravity = [0, 0, -9.80665],
     ):
         """
         Initializes an "identity" IMU RMI.
@@ -84,11 +104,13 @@ class IMUIncrement(RelativeMotionIncrement):
         """
         self.value = np.identity(5)
         self.stamps = [None, None]
-        self.covariance = np.zeros((5, 5))
+        self.covariance = np.zeros((15, 15))
         self.input_covariance = input_covariance
         self.bias_jacobian = np.zeros((9, 6))
-        self.accel_bias = accel_bias
-        self.gyro_bias = gyro_bias
+        self.accel_bias = np.array(accel_bias).ravel()
+        self.gyro_bias = np.array(gyro_bias).ravel()
+        self.gravity = np.array(gravity).ravel()
+
 
     def increment(self, u: IMU, dt: float):
         if self.stamps[0] is None:
@@ -107,7 +129,19 @@ class IMUIncrement(RelativeMotionIncrement):
         A = adjoint_IE3(U_inv)
         L = L_matrix(unbiased_gyro, unbiased_accel, dt)
         Q = self.covariance
-        self.covariance = A @ Q @ A.T + L @ self.input_covariance @ L.T
+
+        A_full = np.zeros((15, 15))
+        A_full[0:9, 0:9] = A
+        A_full[0:9, 9:15] = L
+        A_full[9:15, 9:15] = np.identity(6)
+
+        L_full = np.zeros((15, 12))
+        L_full[0:9, 0:6] = L
+        L_full[9:15, 6:12] = dt*np.identity(6)
+
+        self.covariance = (
+            A_full @ Q @ A_full.T + L_full @ self.input_covariance @ L_full.T
+        )
         self.bias_jacobian = A @ self.bias_jacobian + L
 
     def bias_update(
@@ -129,3 +163,38 @@ class IMUIncrement(RelativeMotionIncrement):
         )
 
         self.value = self.value @ SE23.Exp(self.bias_jacobian @ db)
+
+
+    def predict(self, x: StateWithCovariance) -> StateWithCovariance:
+        """
+        Parameters
+        ----------
+        x : State
+            The state to apply the RMI to
+
+        Returns
+        -------
+        State
+            The updated state
+        """
+        x = x.copy()
+        dt = self.stamps[1] - self.stamps[0]
+        DG = G_matrix(self.gravity, dt)
+        DU = self.value 
+        x_state: IMUState = x.state
+        x_state.pose = DG @ x_state.pose @ DU 
+
+        A = np.identity(15)
+        L = np.identity(15)
+        if x_state.direction == "right":
+            A[0:9, 0:9] = adjoint_IE3(inverse_IE3(DU))
+            A[0:9, 9:15] = self.bias_jacobian
+        elif x_state.direction == "left":
+            Ad = SE23.adjoint(x_state.pose)
+            A[0:9, 0:9] = adjoint_IE3(DG)
+            A[0:9, 9:15] = Ad @ self.bias_jacobian
+            L[0:9, 0:9] = Ad
+
+        x.covariance = A @ x.covariance @ A.T + L @ self.covariance @ L.T
+
+        return x
---
+++ tests/unit/test_preintegration.py
@@ -0,0 +1,56 @@
+
+from pynav.lib.imu import IMU, IMUKinematics, IMUState
+from pynav.lib.preintegration import IMUIncrement
+from pynav.filters import ExtendedKalmanFilter
+import numpy as np 
+from pylie import SE23
+from pynav.types import StateWithCovariance
+
+np.set_printoptions(precision=4, suppress=True, linewidth=200)
+
+def _test_imu_preintegration(direction):
+    """
+    Tests to make sure IMU preintegration and regular dead reckoning
+    are equivalent.
+    """
+    Q = np.identity(12)
+    accel_bias = [1,2,3]
+    gyro_bias = [0.1,0.2,0.3]
+
+    model = IMUKinematics(Q)
+    dt = 0.01
+    u = IMU([1, 2, 3], [2, 3, 1], 0)
+    x = IMUState(
+        SE23.Exp([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+        gyro_bias,
+        accel_bias,
+        0,
+        direction=direction,
+    )
+    P0 = np.identity(15)
+    ekf = ExtendedKalmanFilter(model)
+    x0 = StateWithCovariance(x, P0)
+    x_dr = x0.copy()
+    rmi = IMUIncrement(Q,gyro_bias=gyro_bias,accel_bias=accel_bias)
+
+    # Do both dead reckoning and preintegration
+    for i in range(100):
+        x_dr = ekf.predict(x_dr, u, dt)
+        rmi.increment(u, dt)
+
+    # Apply the rmi to the state
+    x_pre = rmi.predict(x0)
+
+    # Compare the results
+    print(x_dr.covariance - x_pre.covariance)
+    assert np.allclose(x_dr.state.pose, x_pre.state.pose)
+    assert np.allclose(x_dr.covariance, x_pre.covariance)
+
+def test_imu_preintegration_right():
+    _test_imu_preintegration("right")
+
+def test_imu_preintegration_left():
+    _test_imu_preintegration("left")
+
+if __name__ == "__main__":
+    test_imu_preintegration_left()
