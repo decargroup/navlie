d708e5e11d519b75617dc807d0854373987e6bd1
Charles Cossette <charles.cossette@mail.mcgill.ca>
2022-10-21 20:03:18 -0400

    added general "linear" state interpolation method

--- pynav/utils.py
+++ pynav/utils.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Tuple
+from typing import Callable, List, Tuple, Union, Any
 from pynav.types import State, Measurement, StateWithCovariance
 import numpy as np
 import matplotlib.pyplot as plt
@@ -291,6 +291,65 @@ def randvec(cov: np.ndarray, num_samples: int = 1) -> np.ndarray:
     return L @ np.random.normal(0, 1, (cov.shape[0], num_samples))
 
 
+
+
+
+def van_loans(
+    A_c: np.ndarray,
+    L_c: np.ndarray,
+    Q_c: np.ndarray,
+    dt: float,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Van Loan's method for computing the discrete-time A and Q matrices.
+
+    Given a continuous-time system of the form
+
+    .. math::
+        \dot{\mathbf{x}} = \mathbf{A}_c \mathbf{x} + \mathbf{L}_c \mathbf{w}, \hspace{5mm}
+        \mathbf{w} \sim \mathcal{N} (\mathbf{0}, \mathbf{Q}_c ),
+
+    where :math:`\mathbf{Q}_c` is a power spectral density,
+    Van Loan's method can be used to find its equivalent discrete-time representation,
+
+    .. math::
+        \mathbf{x}_k = \mathbf{A}_{d} \mathbf{x}_{k-1} + \mathbf{w}_{k-1}, \hspace{5mm}
+        \mathbf{w} \sim \mathcal{N} (\mathbf{0}, \mathbf{Q}_d ).
+
+    These are computed using the matrix exponential, with a sampling period :math:`\Delta t`.
+
+    Parameters
+    ----------
+    A_c : np.ndarray
+        Continuous-time A matrix.
+    L_c : np.ndarray
+        Continuous-time L matrix.
+    Q_c : np.ndarray
+        Continuous-time noise matrix
+    dt : float
+        Discretization timestep.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        A_d and Q_d, discrete-time matrices.
+    """
+    N = A_c.shape[0]
+
+    A_c = np.atleast_2d(A_c)
+    L_c = np.atleast_2d(L_c)
+    Q_c = np.atleast_2d(Q_c)
+
+    # Form Xi matrix and compute Upsilon using matrix exponential
+    Xi = block_diag(A_c, -A_c.T, A_c, np.zeros((N, N)))
+    Xi[:N, N : 2 * N] = L_c @ Q_c @ L_c.T
+    Upsilon = expm(Xi * dt)
+
+    # Extract relevant parts of Upsilon
+    A_d = Upsilon[:N, :N]
+    Q_d = Upsilon[:N, N : 2 * N] @ A_d.T
+
+    return A_d, Q_d
+
 def plot_error(
     results: GaussianResultList,
     axs: List[plt.Axes] = None,
@@ -426,64 +485,6 @@ def plot_meas(
 
     return fig, axs
 
-
-def van_loans(
-    A_c: np.ndarray,
-    L_c: np.ndarray,
-    Q_c: np.ndarray,
-    dt: float,
-) -> Tuple[np.ndarray, np.ndarray]:
-    """Van Loan's method for computing the discrete-time A and Q matrices.
-
-    Given a continuous-time system of the form
-
-    .. math::
-        \dot{\mathbf{x}} = \mathbf{A}_c \mathbf{x} + \mathbf{L}_c \mathbf{w}, \hspace{5mm}
-        \mathbf{w} \sim \mathcal{N} (\mathbf{0}, \mathbf{Q}_c ),
-
-    where :math:`\mathbf{Q}_c` is a power spectral density,
-    Van Loan's method can be used to find its equivalent discrete-time representation,
-
-    .. math::
-        \mathbf{x}_k = \mathbf{A}_{d} \mathbf{x}_{k-1} + \mathbf{w}_{k-1}, \hspace{5mm}
-        \mathbf{w} \sim \mathcal{N} (\mathbf{0}, \mathbf{Q}_d ).
-
-    These are computed using the matrix exponential, with a sampling period :math:`\Delta t`.
-
-    Parameters
-    ----------
-    A_c : np.ndarray
-        Continuous-time A matrix.
-    L_c : np.ndarray
-        Continuous-time L matrix.
-    Q_c : np.ndarray
-        Continuous-time noise matrix
-    dt : float
-        Discretization timestep.
-
-    Returns
-    -------
-    Tuple[np.ndarray, np.ndarray]
-        A_d and Q_d, discrete-time matrices.
-    """
-    N = A_c.shape[0]
-
-    A_c = np.atleast_2d(A_c)
-    L_c = np.atleast_2d(L_c)
-    Q_c = np.atleast_2d(Q_c)
-
-    # Form Xi matrix and compute Upsilon using matrix exponential
-    Xi = block_diag(A_c, -A_c.T, A_c, np.zeros((N, N)))
-    Xi[:N, N : 2 * N] = L_c @ Q_c @ L_c.T
-    Upsilon = expm(Xi * dt)
-
-    # Extract relevant parts of Upsilon
-    A_d = Upsilon[:N, :N]
-    Q_d = Upsilon[:N, N : 2 * N] @ A_d.T
-
-    return A_d, Q_d
-
-
 def plot_poses(
     poses: List[SE3State],
     ax: plt.Axes = None,
@@ -517,6 +518,8 @@ def plot_poses(
     if ax is None:
         fig = plt.figure()
         ax = plt.axes(projection="3d")
+    else:
+        fig = ax.get_figure()
 
     if triad_color is None:
         colors = ["tab:red", "tab:green", "tab:blue"] # Default to RGB
@@ -531,12 +534,12 @@ def plot_poses(
     C = np.array([poses[i].attitude.T for i in range(0, len(poses), step)])
     r = np.array([poses[i].position for i in range(0, len(poses), step)])
     x, y, z = r[:, 0], r[:, 1], r[:, 2]
-    ax.quiver(x, y, z, C[:, 0, 0], C[:, 0, 1], C[:, 0, 2], color=colors[0], length=arrow_length)
-    ax.quiver(x, y, z, C[:, 1, 0], C[:, 1, 1], C[:, 1, 2], color=colors[1], length=arrow_length)
-    ax.quiver(x, y, z, C[:, 2, 0], C[:, 2, 1], C[:, 2, 2], color=colors[2], length=arrow_length)
+    ax.quiver(x, y, z, C[:, 0, 0], C[:, 0, 1], C[:, 0, 2], color=colors[0], length=arrow_length, arrow_length_ratio=0.1)
+    ax.quiver(x, y, z, C[:, 1, 0], C[:, 1, 1], C[:, 1, 2], color=colors[1], length=arrow_length, arrow_length_ratio=0.1)
+    ax.quiver(x, y, z, C[:, 2, 0], C[:, 2, 1], C[:, 2, 2], color=colors[2], length=arrow_length, arrow_length_ratio=0.1)
 
     set_axes_equal(ax)
-
+    return fig, ax
 
 def set_axes_equal(ax: plt.Axes):
     """Sets the axes of a 3D plot to have equal scale.
@@ -560,3 +563,43 @@ def set_axes_equal(ax: plt.Axes):
     ax.set_xlim3d([x_middle - length, x_middle + length])
     ax.set_ylim3d([y_middle - length, y_middle + length])
     ax.set_zlim3d([z_middle - length, z_middle + length])
+
+def state_interp(stamps: Union[float, List[float], Any], state_list: List[State]):
+    # TODO: add tests
+    # Handle input
+    if not isinstance(stamps, list):
+        stamps = [stamps]
+        single_query = True
+    else:
+        single_query = False
+
+    stamps = stamps.copy()
+    for i, stamp in enumerate(stamps):
+        if not isinstance(stamp, float):
+            if hasattr(stamp, "stamp"):
+                stamp = stamp.stamp
+                stamps[i] = stamp
+            else:
+                raise TypeError("Stamps must be of type float or have a stamp attribute")
+        
+
+    # Get the indices of the states just before and just after.
+    state_list = np.array(state_list)
+    stamp_list = [state.stamp for state in state_list]
+    stamp_list.sort()
+    stamp_list = np.array(stamp_list)
+    idx_middle = np.interp(stamps, stamp_list, np.array(range(len(stamp_list))))
+    idx_lower = np.floor(idx_middle).astype(int)
+    idx_upper = idx_lower + 1
+
+    # Do the interpolation
+    stamp_lower = stamp_list[idx_lower]
+    stamp_upper = stamp_list[idx_upper]
+    alpha = np.array((stamps - stamp_lower) / (stamp_upper - stamp_lower)).ravel()
+    state_lower: List[State] = np.array(state_list[idx_lower]).ravel()
+    state_upper: List[State] = np.array(state_list[idx_upper]).ravel()
+    dx = np.array([s.minus(state_lower[i]).ravel() for i, s in enumerate(state_upper)])
+    out =  [s.plus( alpha[i]* dx[i]) for i, s in enumerate(state_lower)]
+    if single_query:
+        out = out[0]
+    return out
