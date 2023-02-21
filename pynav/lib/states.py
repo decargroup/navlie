Fixed bug with jacobian() util.

pynav/lib/states.py
@@ -48,7 +48,7 @@ class MatrixLieGroupState(State):
     The MatrixLieGroupState class. Although this class can be used directly,
     it is recommended to use one of the subclasses, such as SE2State or SO3State.
     """
-
+    # TODO. Add identity() and random() functions to this
     __slots__ = ["group", "direction"]
 
     def __init__(
@@ -82,8 +82,16 @@ class MatrixLieGroupState(State):
 
                 \mathbf{X} = \exp(\delta \mathbf{x}^\wedge) \mathbf{X} \text{ (left)}
         """
+        if isinstance(value, list):
+            value = np.array(value)
+
         if value.size == group.dof:
             value = group.Exp(value)
+        elif value.shape[0] != value.shape[1]:
+            raise ValueError(
+                f"value must either be a {group.dof}-length vector of exponential"
+                "coordinates or a matrix direct element of the group."
+            )
 
         self.direction = direction
         self.group = group
@@ -147,7 +155,7 @@ class MatrixLieGroupState(State):
             f"{value_str}",
         ]
         return "\n".join(s)
-
+    
     @property
     def attitude(self) -> np.ndarray:
         raise NotImplementedError(
@@ -431,9 +439,6 @@ class SE23State(MatrixLieGroupState):
         state_id=None,
         direction="right",
     ):
-        if value.shape != (5, 5):
-            raise ValueError("Value must be a 5x5 matrix")
-
         super().__init__(value, SE23, stamp, state_id, direction)
 
     @property

pynav/utils.py
@@ -1145,7 +1145,7 @@ def find_nearest_stamp_idx(stamps_list: List[float], stamp: Union[float, List[fl
 def jacobian(
     fun: Callable,
     x: Union[np.ndarray, State],
-    step_size=1e-6,
+    step_size=None,
     method="forward",
     *args,
     **kwargs,
@@ -1190,6 +1190,12 @@ def jacobian(
     """
     x = x.copy() 
 
+    if step_size is None:
+        if method=="cs":
+            step_size = 1e-16
+        else:
+            step_size = 1e-6
+
     # Check if input has a plus method. otherwise, assume it will behave
     # like a numpy array
     if hasattr(x, "plus"):
@@ -1206,6 +1212,10 @@ def jacobian(
     else:
         output_diff = lambda Y, Y_bar: Y - Y_bar
 
+
+
+    func_to_diff = lambda dx : output_diff(fun(input_plus(x, dx), *args, **kwargs), Y_bar)
+
     # Check if input/output has a dof attribute. otherwise, assume it will
     # behave like a numpy array and use the `.size` attribute to get
     # the DOF of the input/output
@@ -1219,28 +1229,31 @@ def jacobian(
     else:
         M = Y_bar.size
 
+
+    Y_bar_diff = func_to_diff(np.zeros((N,)))
     jac_fd = np.zeros((M, N))
 
     # Main loop to calculate jacobian
     for i in range(N):
         dx = np.zeros((N))
+        dx[i] = step_size
 
         if method == "forward":
-            dx[i] = step_size
-            Y_plus: State = fun(input_plus(x, dx).copy(), *args, **kwargs)
-            jac_fd[:, i] = output_diff(Y_plus, Y_bar).ravel() / step_size
+            Y_plus: State = func_to_diff(dx.copy())
+            jac_fd[:, i] = (Y_plus - Y_bar_diff).ravel() / step_size
 
         elif method == "central":
-            dx[i] = step_size
-            Y_plus: State = fun(input_plus(x, dx).copy(), *args, **kwargs)
-            Y_minus: State = fun(input_plus(x, -dx).copy(), *args, **kwargs)
-            jac_fd[:, i] = output_diff(Y_plus, Y_minus).ravel() / (2*step_size)
+            Y_plus = func_to_diff(dx.copy())
+            Y_minus = func_to_diff(-dx.copy())
+            jac_fd[:, i] = (Y_plus - Y_minus).ravel() / (2*step_size)
 
         elif method == "cs":
-            dx[i] = 1e-16
-            Y_imag: State = fun(input_plus(x, 1j*dx).copy(), *args, **kwargs)
-            jac_fd[:, i] = np.imag(Y_imag).ravel() / 1e-16
+            Y_imag: State = func_to_diff(1j*dx.copy())
+            jac_fd[:, i] = np.imag(Y_imag).ravel() / step_size
 
+        else:
+            raise ValueError(f"Unknown method '{method}'. "
+                             "Must be 'forward', 'central' or 'cs")
 
 
     return jac_fd