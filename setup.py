aa44e73e30f2e05dafedb3639f2e2986bc7498f4
Charles Cossette <charles.cossette@mail.mcgill.ca>
2023-03-28 13:09:42 -0400

    Added more deps

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
         "scipy>=1.7.1",
         "matplotlib>=3.4.3",
         "joblib>=1.2.0",
-        'pylie @ git+https://github.com/decargroup/pylie@main'
-    ]
+        "pylie @ git+https://github.com/decargroup/pylie@main",
+        "tqdm>=4.64.1",
+        "seaborn>=0.11.2",
+    ],
 )
